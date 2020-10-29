3d63e1b5a5d4e719dd5ddb41a9f8b939429b0f8e
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-29 17:02:27 -0600

    trabajando con el metodo index

---
+++ app/Http/Controllers/RecetaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Receta;
+use Illuminate\Http\Request;
+
+class RecetaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('recetas.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Receta $receta)
+    {
+        //
+    }
+}
---
+++ resources/views/recetas/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+    <h2 class="text-center md-5">Administra tus recetas</h2>
+    <div class="col-md-10 mx-auto bg-white p-3">
+        <table class="table">
+            <thead class="bg-primary text-light">
+                <tr>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Pizza</td>
+                    <td>Pizzas</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
