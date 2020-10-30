f9a3203ac21b634cbb17f68ccd3e9dd34b2689ea
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-30 12:31:56 -0600

    Creando el resto de campos para recetas en la migracion

--- database/migrations/2020_10_29_224153_create_recetas_table.php
+++ database/migrations/2020_10_29_224153_create_recetas_table.php
@@ -13,9 +13,20 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+        Schema::create('categoria_receta',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            $table->foreignId('user_id')->references('id')->on('users')->comment('El usuario que crea la receta');
+            $table->foreignId('categoria_id')->references('id')->on('categoria_receta')->comment('La categoria de la receta');
             $table->timestamps();
         });
     }
@@ -27,6 +38,7 @@ class CreateRecetasTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('categoria_receta');
         Schema::dropIfExists('recetas');
     }
 }
