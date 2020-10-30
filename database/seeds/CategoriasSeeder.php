42d93ae35b14f3aa4b0fd42827061a988abf7d5b
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-30 12:49:51 -0600

    Creando un seed para categorias

---
+++ database/seeds/CategoriasSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida Mexicana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida Argentina',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida mexicana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Postres',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Bebidas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Cortes',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Ensaladas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Desayunos',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
