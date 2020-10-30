84dc25688488ebfee0d4faaab50a8329116fd014
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-30 13:42:18 -0600

    agregando seeder para usuarios

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Julio Cesar',
+            'email' =>'vcjuliocesar@faker.com',
+            'password' => Hash::make('12345678'),
+            'url'=>'http://miweb.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Bruce Wayne',
+            'email' =>'batman@baticorreo.com',
+            'password' => Hash::make('12345678'),
+            'url'=>'http://batman.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
