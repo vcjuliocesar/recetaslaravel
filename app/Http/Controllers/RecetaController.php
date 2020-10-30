1aba0ecdc6c2f0672318cb2489466a9e9a0ffaea
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-30 12:12:32 -0600

    proteger url

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\DB;
 
 class RecetaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
