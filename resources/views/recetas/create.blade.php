cba84a64f99f99b87ae91f47918f22f5c01455f7
vcjuliocesar <vcjuliocesar@outlook.com>
2020-10-30 10:15:40 -0600

    Formulario para crear recetas

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -6,5 +6,23 @@
 
 @section('content')
     <h2 class="text-center md-5">Crear nueva receta</h2>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="titulo"> Titulo Receta</label>
+                    <input type="text"
+                        name="titulo"
+                        class="form-control"
+                        id="titulo"
+                        placeholder="Titulo Receta"
+                    />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta">
+                </div>
+            </form>
+        </div>
+    </div>
 
 @endsection
