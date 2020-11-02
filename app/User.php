80fe965ea7a2dc4937300611907c81e0712bd513
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-02 10:35:37 -0600

    Creando relacion 1:n entre Usuarios y Recetas

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /** relacion 1:n Usuario a recetas*/
+    public function recetas()
+    {
+        return $this->hasMany(Receta::class);
+    }
 }
