f6c7c9244a96dae6d48f0df187af83323b3e5393
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-03 17:43:57 -0600

    Agregar in enlace para ver Recetas desde el Panel

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -24,7 +24,7 @@
                         <td>
                             <a href="" class="btn btn-danger mr-1">Eliminar</a>
                             <a href="" class="btn btn-dark mr-1">Editar</a>
-                            <a href="" class="btn btn-success mr-1">Ver</a>
+                            <a href="{{ route('recetas.show',['receta'=>$receta->id]) }}" class="btn btn-success mr-1">Ver</a>
                         </td>
                     </tr>
                 @endforeach
