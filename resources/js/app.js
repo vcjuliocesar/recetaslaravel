9df0ebcacf9ca1e6190403482bcafff2de6692e3
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-06 16:51:31 -0600

    Agregando Sweet Alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+import VueSweetalert2 from 'vue-sweetalert2';
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -18,7 +18,7 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
+Vue.use(VueSweetalert2);
 //Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.config.ignoredElements = ['trix-editor','trix-toolbar'];
 Vue.component('fecha-receta',require('./components/FechaReceta.vue').default);
--- resources/js/components/EliminarReceta.vue
+++ resources/js/components/EliminarReceta.vue
@@ -4,16 +4,35 @@
         class="btn btn-danger d-block w-100 mb-2"
         value="Eliminar x"
         @click="eliminarReceta"
-    >
+    />
 </template>
 
 <script>
 export default {
-    props:['recetaId'],
-    methods:{
-        eliminarReceta(){
-            console.log('click',this.recetaId);
+    props: ["recetaId"],
+    methods: {
+        eliminarReceta() {
+            this.$swal({
+                title: "¿Deseas eliminar esta receta?",
+                text: "Una vez eliminada, no se puede recuperar",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Si",
+                cancelButtonText:"No"
+            }).then(result => {
+                if (result.isConfirmed) {
+
+                    //enviar la petición al servidor
+                    this.$swal({
+                        title :"Receta eliminada",
+                        text : "Se eliminó la receta",
+                        icon : "success"
+                    });
+                }
+            });
         }
     }
-}
+};
 </script>
