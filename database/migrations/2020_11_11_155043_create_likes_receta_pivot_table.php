208c3a8dfc6160109be2a9771e111af7a6f2ace7
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-11 10:31:08 -0600

    Creando una tabla pivote para almacenar los Likes

---
+++ database/migrations/2020_11_11_155043_create_likes_receta_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesRecetaPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes_receta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('receta_id')->constrained();//esto es igual a = references('id')->on('recetas')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes_receta');
+    }
+}
