95912558423e5653930522d0edd6208157a707b5
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-11 11:57:39 -0600

    Almacenar o quitar los likes con un toggle

---
+++ app/Http/Controllers/LikesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Receta;
+use Illuminate\Http\Request;
+
+class LikesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Receta $receta)
+    {
+        // ALmacena los likes de un usuario a una receta
+        return auth()->user()->meGusta()->toggle($receta);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,7 @@ Route::get('/perfiles/{perfil}','PerfilController@show')->name('perfiles.show');
 Route::get('/perfiles/{perfil}/edit','PerfilController@edit')->name('perfiles.edit');
 Route::put('/perfiles/{perfil}','PerfilController@update')->name('perfiles.update');
 
+// Almacena los likes de las recetas
+Route::post('recetas/{receta}','LikesController@store')->name('likes.store');
+
 Auth::routes();
