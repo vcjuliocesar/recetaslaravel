3bc0fc4dd98ae046743e36badb806c8d6f769ad0
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-12 13:23:18 -0600

    Obtener recetas por su categoria

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Receta;
+use App\CategoriaReceta;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -11,6 +13,17 @@ class InicioController extends Controller
     {
         //Obtiene las recetas mas nuevas
         $nuevas = Receta::latest()->take(5)->get();
+
+        // obtener todas las categorias
+        $categorias = CategoriaReceta::all();
+
+        // Agrupar las Recetas por Categoria
+        $recetas = [];
+
+        foreach($categorias as $categoria){
+            $recetas[Str::slug($categoria->nombre)][] = Receta::where('categoria_id',$categoria->id)->take(3)->get();
+        }
+
         return view('inicio.index',compact('nuevas'));
     }
 }
