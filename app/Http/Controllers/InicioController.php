30ade5657e917698679e7deb594680da5b5aed27
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-12 09:18:46 -0600

    Obteniendo las ultimas recetas

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Receta;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
 {
     public function index()
     {
-        return view('inicio.index');
+        //Obtiene las recetas mas nuevas
+        $nuevas = Receta::latest()->take(5)->get();
+        return view('inicio.index',compact('nuevas'));
     }
 }
